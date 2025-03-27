186fac026f2a0312df5cc543e78975b5e117d220
Luke Lambert <luke.lambert.1165@gmail.com>
2025-03-27 09:50:31 -0500

    adding lidar and camera

--- tracer/tracer_gazebo_sim/launch/tracer_full_sim.launch.py
+++ tracer/tracer_gazebo_sim/launch/tracer_full_sim.launch.py
@@ -1,8 +1,21 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import ExecuteProcess
+from launch.actions import ExecuteProcess, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
+    tracer_description_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('tracer_description'),
+                'launch',
+                'display_tracer.launch.py'
+            )
+        )
+    )
+
     return LaunchDescription([
         # Start Gazebo server
         ExecuteProcess(
@@ -15,19 +28,14 @@ def generate_launch_description():
             output='screen'
         ),
 
-        # Start Gazebo client (GUI)
+        # Start Gazebo client
         ExecuteProcess(
             cmd=['gzclient', '--verbose'],
             output='screen'
         ),
 
-        # Robot state publisher and RViz
-        Node(
-            package='tracer_description',
-            executable='display_tracer.launch.py',
-            name='display_tracer',
-            output='screen',
-        ),
+        # Include RViz + robot state publisher
+        tracer_description_launch,
 
         # Spawn the robot
         Node(
